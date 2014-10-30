2f7d7764efc5ff68d7b3f9d04d897f4287dbb240
Adam Coddington <adam.coddington@coxinc.com>
2014-10-29 22:02:50 -0700

    Release 1.0.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ except IOError:
 
 setup(
     name="ircpdb",
-    version="1.0.1",
+    version="1.0.2",
     description="Remotely debug your Python application via IRC.",
     long_description=README + "\n\n" + CHANGES,
     author="Adam Coddington",
