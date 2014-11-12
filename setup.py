37501472862f8edbc476e54e59a045519532c25b
Adam Coddington <adam.coddington@coxinc.com>
2014-11-11 19:24:13 -0800

    Release 1.4; better prompt and message handling; restore Py2.6 support by pinning irc library version at an older release.

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ except IOError:
 
 setup(
     name="ircpdb",
-    version="1.3",
+    version="1.4",
     description=(
         "Remotely and collaboratively debug your Python application via IRC"
     ),
